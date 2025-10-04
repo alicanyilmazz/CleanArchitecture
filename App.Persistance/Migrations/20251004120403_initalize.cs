0c2e4d523802d82e95ac1536bd9d5c883ac3f366
alican yılmaz <alicanyilmaz101@gmail.com>
2025-10-04 15:06:21 +0300

    Migration created for initializing database

---
+++ App.Persistance/Migrations/20251004120403_initalize.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class initalize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                    table.CheckConstraint("CK_Product_Price_NonNegative", "[Price] >= 0");
+                    table.CheckConstraint("CK_Product_Quantity_NonNegative", "[Quantity] >= 0");
+                },
+                comment: "Products catalog.");
+
+            migrationBuilder.CreateIndex(
+                name: "UX_Product_Name",
+                table: "Product",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
